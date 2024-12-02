125b8f7e09a2447715ff35ce0f45a6e5756995c9
magnus012 <igor_012@hotmail.com>
2024-12-02 11:44:10 -0300

    ghpage

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<TelaHome />} />
-          <Route path="/usuario" element={<TelaUsuario />} />
-          <Route path="/mensagem" element={<TelaMensagem />} />
+          <Route path="/Prova2BimestreBCCLP2/" element={<TelaHome />} />
+          <Route path="/Prova2BimestreBCCLP2/usuario" element={<TelaUsuario />} />
+          <Route path="/Prova2BimestreBCCLP2/mensagem" element={<TelaMensagem />} />
           <Route path="*" element={<Tela404 />} />
         </Routes>
       </BrowserRouter>
